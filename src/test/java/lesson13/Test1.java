5d6d77d967638920cc203048e8a685d7cf8d077f
VValigura <Volodymyr.valigura@gmail.com>
2022-04-19 13:57:46 +0300

    add jUnit

--- src/test/java/lesson13/Test1.java
+++ src/test/java/lesson13/Test1.java
@@ -1,17 +1,44 @@
 package lesson13;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class Test1 {
-    public static void main(String[] args) {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start(){
         System.setProperty("webdriver.chrome.driver", "C:\\WebDrivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
-        driver.navigate().to("https://www.google.com.ua/");
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void test(){
         driver.navigate().to("https://www.google.com.ua/");
         driver.findElement(By.name("q")).sendKeys("WebDriver");
+    }
+
+
+
+    @After
+    public void stop(){
         driver.quit();
     }
+
+
+
+
+
 
 }
